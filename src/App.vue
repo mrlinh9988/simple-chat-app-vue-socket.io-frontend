48ea47e538f23e6f8d00e298915969826120beed
mrlinh <mrlinh@gmail.com>
2020-08-31 11:14:09 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header">
+      <h1>Chatroom</h1>
+      <p class="username">Username: {{ username }}</p>
+      <p class="online">Online: {{ users.length }}</p>
     </div>
-    <router-view/>
+    <ChatRoom :messages="messages" v-on:sendMessage="this.sendMessage" />
   </div>
 </template>
 
-<style>
+<script>
+import io from "socket.io-client";
+import ChatRoom from "@/components/ChatRoom.vue";
+
+export default {
+  data() {
+    return {
+      username: "",
+      socket: io("http://localhost:3000"),
+      messages: [],
+      users: [],
+    };
+  },
+  components: { ChatRoom },
+  methods: {
+    joinServer() {
+      this.socket.on("loggedIn", (data) => {
+        this.messages = data.messages;
+        this.users = data.users;
+        this.socket.emit("newUser", this.username);
+      });
+
+      this.listen();
+    },
+    listen() {
+      this.socket.on("userOnline", (user) => {
+        this.users.push(user);
+      });
+
+      this.socket.on("userLeft", (user) => {
+        this.users.splice(this.users.indexOf(user), 1);
+      });
+
+      this.socket.on("msg", (message) => {
+        this.messages.push(message);
+      });
+    },
+    sendMessage(message) {
+      console.log(message);
+      this.socket.emit("msg", message);
+    },
+  },
+  mounted() {
+    this.username = prompt("What is your username?", "AnonymousS");
+
+    if (!this.username) {
+      this.username = "Anonymous";
+    }
+
+    this.joinServer();
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
 }
 </style>
